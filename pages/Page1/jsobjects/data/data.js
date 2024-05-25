53510927ed5b73104eeb5e3890553ac353bcde25
Stafford Shealy <sshealy@gmail.com>
2024-05-25 18:26:17 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/data/data.js
@@ -0,0 +1,8 @@
+export default {
+	todos: undefined,
+	dummytodos: [
+		{titles: 'Use Input to Add Todo', completed: true},
+		{titles: 'Click Hit to update', completed: false},
+		{titles: 'Click on Done Todo', completed: false},
+		{titles: 'Have Fun', completed: false}],
+}
